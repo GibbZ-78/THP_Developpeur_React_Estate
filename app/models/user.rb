d14ea6908bff3a4037cb66cbe4862f711de07aff
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-14 17:22:55 +0200

    property model & migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_many :properties
+
   validates :email, presence: true, uniqueness: true, length: { minimum: 3 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
---
+++ db/migrate/20220614151458_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :properties do |t|
+      t.string :title,
+      t.float :price,
+      t.text :description,
+      t.belongs_to :property_type,
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
