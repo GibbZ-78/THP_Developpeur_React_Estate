08a654a1ae6df5abce1b6f72f680a23ca55f9482
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-14 17:02:40 +0200

    email presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true, length: { minimum: 3 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
