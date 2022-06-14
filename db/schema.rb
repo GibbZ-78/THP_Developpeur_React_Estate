321b043be5e6b1d75d8e5f9ddbb8c4e2615a050a
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-14 17:47:44 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_143029) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_153855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "private_messages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "sender_id"
+    t.bigint "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_private_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_private_messages_on_sender_id"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.text "description"
+    t.bigint "property_type_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_type_id"], name: "index_properties_on_property_type_id"
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
+  create_table "property_types", force: :cascade do |t|
+    t.string "property_type_title"
+    t.text "property_type_desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recommendations", force: :cascade do |t|
+    t.text "content"
+    t.bigint "author_id"
+    t.bigint "target_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_recommendations_on_author_id"
+    t.index ["target_id"], name: "index_recommendations_on_target_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
