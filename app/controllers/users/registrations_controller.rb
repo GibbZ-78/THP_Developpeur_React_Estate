df101f1dad09d76ecba3400ab8b47c2a05adf40b
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-16 14:59:02 +0200

    syntax

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,6 +17,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def register_failed
-    render json: { message: "Something went wrong." }, stauts: :unprocessable_entity
+    render json: { message: "Something went wrong." }, status: :unprocessable_entity
   end
 end
