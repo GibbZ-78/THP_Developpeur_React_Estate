5a290544110f50b20acdd3ac9876c9e4c604b0f4
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-16 08:52:23 +0200

    jwt devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_153855) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_062559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "private_messages", force: :cascade do |t|
     t.string "title"
