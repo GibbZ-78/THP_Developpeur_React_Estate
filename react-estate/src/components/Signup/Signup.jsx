3e6a46b2b39b7efab81f1b18460be48da049db40
Teacher-droid <teaching.know@gmail.com>
2022-06-17 08:51:12 +0200

    adding Footer, Header, Hero, Button, Cards, CardItem, About, Contact components and customized CSS for almost each of those,though it will probably been needed to rebuild the Home component into App.js or simply deleting it

--- react-estate/src/components/Signup/Signup.jsx
+++ react-estate/src/components/Signup/Signup.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../App.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -7,10 +8,10 @@ const Signup = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  function onFormSubmit(e){
+  function onFormSubmit(e) {
     e.preventDefault();
     const data = {
-      "user" :{
+      "user": {
         "email": email,
         "password": password
       }
@@ -23,39 +24,39 @@ const Signup = () => {
       },
       body: JSON.stringify(data)
     })
-    .then((response) => {return response.json()})
-    .then((response) => {
-      fetch('http://localhost:3000/users/sign_in', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+      .then((response) => { return response.json() })
+      .then((response) => {
+        fetch('http://localhost:3000/users/sign_in', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
       })
-    })
-}
-    return (
-      <Form onSubmit={onFormSubmit}>
+  }
+  return (
+    <Form onSubmit={onFormSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="text" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/>
-      <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-      </Form.Text>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="text" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
+        <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
       <Button variant="primary" type="submit">
-      Submit
+        Submit
       </Button>
-      </Form>
-      );
-  }
+    </Form>
+  );
+}
 
-  export default Signup;
+export default Signup;
